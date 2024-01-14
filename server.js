3e58247008a22efbe4506c9cac1dd62595119699
DaniOwnerShip <daniorcha@hotmail.com>
2024-01-14 04:35:59 +0100

    add api

--- server.js
+++ server.js
@@ -23,25 +23,25 @@ server.use(cors());
 server.use(bodyParser.json());
 
 //const uri = `mongodb+srv://${username}:${password}@cluster0.8y0ptqu.mongodb.net/${dbName}`;
-const uri = `mongodb://localhost:27017/teamTest`; 
+// const uri = `mongodb://localhost:27017/teamTest`; 
 
-(async () => {
-  try {
-    await mongoose.connect(uri, {});
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    return;
-  }
-})();
+// (async () => {
+//   try {
+//     await mongoose.connect(uri, {});
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//     return;
+//   }
+// })();
 
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB err:', err);
-});
+// mongoose.connection.on('error', (err) => {
+//   console.error('MongoDB err:', err);
+// });
 
-mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB disconnected');
-});
+// mongoose.connection.on('disconnected', () => {
+//   console.log('MongoDB disconnected');
+// });
 
 server.get('/testindex', async (req, res) => {
   console.log('get test'  ); 
